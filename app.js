b002b3fbb55c9b7294216e99993990bcc01abdf2
Zetzher <fyeah16@gmail.com>
2020-03-21 19:49:33 +0100

    Middleware arreglado y path's con router realizados correctamente

--- app.js
+++ app.js
@@ -53,11 +53,11 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 //////////////////// para los middleware
-/*app.use(require('node-sass-middleware')({
+app.use(require('node-sass-middleware')({
   src:  path.join(__dirname, 'public'),
   dest: path.join(__dirname, 'public'),
   sourceMap: true
-}));*/
+}));
 ////////////////////
 
 
