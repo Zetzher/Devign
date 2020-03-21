0b8e630db29c8392a64d0d8a6d2b5eb522a507b9
Zetzher <fyeah16@gmail.com>
2020-03-21 18:54:34 +0100

    Index estructurado

--- app.js
+++ app.js
@@ -60,9 +60,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 }));*/
 ////////////////////
 
-/*
+
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
-*/
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
