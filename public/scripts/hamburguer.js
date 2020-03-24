335e5649625fcf7c39e25d7556dfbe9d7ce72ffe
Zetzher <fyeah16@gmail.com>
2020-03-24 20:51:17 +0100

    Navbar responsive con hamburguer

--- public/scripts/hamburguer.js
+++ public/scripts/hamburguer.js
@@ -1,7 +1,9 @@
-/*Hide menu by default*/
-$("#menu").hide();
-
-/*When menu button is clicked, toggle the menu*/
-$("#menu-btn").click(function(){
-  $("#menu").slideToggle();
-});
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
