41506aa277b9afa71b6176f7d0f9c1664d7296b6
Zetzher <fyeah16@gmail.com>
2020-03-27 11:45:56 +0100

    Arreglado perfil

--- routes/private/user.js
+++ routes/private/user.js
@@ -31,17 +31,30 @@ router.get('/', (req, res, next) => {
 
 });
 
-router.post('/', uploadCloud.single('photo'), (req, res, next) => {
-	const imgPath = req.file.url;
+router.post('/', uploadCloud.single('photo'),  (req, res, next) => {
 	
 
-	User.findByIdAndUpdate(req.session.currentUser._id, { imgPath: imgPath })
+	if(req.file){
+		const imgPath = req.file.url;
+		
+	 User.findByIdAndUpdate(req.session.currentUser._id, { imgPath: imgPath })
 		.then(user => {
 			res.redirect('/private/user');
 		})
 		.catch(error => {
 			console.log(error);
 		});
+	} else {
+		res.redirect('/private/user')
+	}
 });
+
+// router.post('/:id/edit-profile',uploadCloud.single('picture'), async (req, res, next) => {
+//     const { name, email, direction} = req.body;
+//     const profilePicture = req.file ? req.file.secure_url : req.session.currentUser.picture;
+//     //console.log(req.file);
+//     await User.update({_id: req.params.id}, {name, email, direction, picture: profilePicture},{new:true})
+//     res.redirect('/profile');
+// });
 
 module.exports = router;
